a363bdbaa086e06637edf8bb63451cacd71f05e6
Anandu S Nande <anandhu66330@gmail.com>
2022-07-14 12:52:00 +0530

    create class base and function base views and urls and create serializer.serializer, next create Model.serializer

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,21 +1,43 @@
-
 from rest_framework import serializers
 from watchlist.models import Movie
 
 
-class MovieSerializer(serializers.Serializer):
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        field = "__all__"
 
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    active = serializers.BooleanField()
 
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)  #this is for POST method
 
-    def update(self, instance, validated_data): ## this is for PUT method
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('cative', instance.active)
-        instance.save()
-        return instance
+
+##############################THIS IS SERIALIZER.SERIALIZER####################################
+
+# class MovieSerializer(serializers.Serializer):
+
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)  #this is for POST method
+
+#     def update(self, instance, validated_data): ## this is for PUT method
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('cative', instance.active)
+#         instance.save()
+#         return instance
+
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError("Title and description should be different")  ##validation by field
+#         else:
+#             return data
+
+
+#     def validate_name(self, value):  
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")  ### validation by object
+#         else:
+#             return value
--- watchlist/api/urls.py
+++ watchlist/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
-from watchlist.api.views import movie_details , movie_list
+#from watchlist.api.views import movie_details , movie_list
+from watchlist.api.views import MovieListAV, MovieDetailAV
 #from . import views
 
 urlpatterns = [
-    path('list/', movie_list, name="movie_list"),
-    path('<int:pk>', movie_details, name="movie_deatils"),
+    path('list/', MovieListAV.as_view(), name="movie_list"),
+    path('<int:pk>', MovieDetailAV.as_view(), name="movie_deatils"), ### THis is class base url
 ]
 
 # from django.urls import path
@@ -12,7 +13,7 @@ urlpatterns = [
 # from .views import movie_details, movie_list
 
 # urlpatterns = [
-#     path('list/', movie_list, name="movie_list"),
+#     path('list/', movie_list, name="movie_list"),          ###This is fucntion base url
 #     path('<int:pk>',movie_details, name="movie_deatils"),
 # ]
 
